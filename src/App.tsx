1c1b44ee22db23bf63fabb42f678d21e0fab26ad
Amina-benhassine <benhassine.amina@hotmail.com>
2024-10-22 15:10:43 +0200

    Ouais c'est méchant j'ai tout refait du début

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import PokemonCard from "./components/pokemonCard.tsx";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <PokemonCard />
+    </div>
+  );
+}
+
+export default App;
